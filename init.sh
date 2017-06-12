ad0ac30495d8b2ecc2debd6a8f131797a585161a
JamesDaniel <jamesmcgarr@protonmail.com>
2017-06-12 12:47:06 +0100

    setup git in init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,12 @@
+# add support for exfat
 sudo apt-get update
 sudo apt-add-repository ppa:relan/exfat
 sudo apt-get update
 sudo apt-get install fuse
 sudo apt-get install exfat-fuse
 sudo apt-get install exfat-utils
+
+# setup git. install it before hand.
+git config --global user.email "jamesmcgarr@protonmail.com"
+git config --global user.name "JamesDaniel"
 
