9a0534ed512c13a804ccd1393e1c129d287bfd22
JamesDaniel <jamesmcgarr@protonmail.com>
2017-06-12 16:58:08 +0100

    Install NodeJS nodejs_install.sh Fixed problem with exfat ppa in init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,10 @@
 # add support for exfat
 sudo apt-get update
-sudo apt-add-repository ppa:relan/exfat
+
+# sudo apt-add-repository ppa:relan/exfat # This causes problems later.
+# sudo add-apt-repository -r ppa:relan/exfat
+# sudo apt-get update
+
 sudo apt-get update
 sudo apt-get install fuse
 sudo apt-get install exfat-fuse
