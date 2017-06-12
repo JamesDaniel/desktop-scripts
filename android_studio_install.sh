d789715da096dc1b8ac3de639c0b5b18643c4ee6
JamesDaniel <jamesmcgarr@protonmail.com>
2017-06-12 16:19:52 +0100

    install android studio android_studio_install.sh

---
+++ android_studio_install.sh
@@ -0,0 +1,5 @@
+wget https://dl.google.com/dl/android/studio/ide-zips/2.3.3.0/android-studio-ide-162.4069837-linux.zip
+unzip android-studio-ide-162.4069837-linux.zip
+rm android-studio-ide-162.4069837-linux.zip
+sudo mv android-studio/ /usr/share/android-studio/
+/usr/share/android-studio/bin/studio.sh
