d410134630b258eebb47fca5aab31a6be66ba08e
JamesDaniel <jamesmcgarr@protonmail.com>
2017-06-15 09:46:05 +0100

    update postgres container script postgresql_start.sh

--- postgresql_start.sh
+++ postgresql_start.sh
@@ -11,3 +11,6 @@ password: mysecretpassword
 The connection can be tested with DataGrip by jetbrains.
 
 '
+
+# Alternative arguments
+# docker run --name postgres -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=my_database -d -p 5432:5432 -v /opt/postgres-data:/var/lib/postgresql/data postgres
