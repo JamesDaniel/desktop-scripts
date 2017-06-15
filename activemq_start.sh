fa817fffb9e82c999ab33a8c6e9161397fb7c88b
JamesDaniel <jamesmcgarr@protonmail.com>
2017-06-15 09:31:58 +0100

    fixed activemq

--- activemq_start.sh
+++ activemq_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker build -t webcenter/activemq /home/$(whoami)/desktop-scripts/active/
-docker run --name='activemq' -it -d --rm -p 8161:8161 webcenter/activemq
+docker run --name='activemq' -it -d --rm -p 8161:8161 -p 61616:61616 webcenter/activemq
