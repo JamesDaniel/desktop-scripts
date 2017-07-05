8055a18323730e64f9438fae2a635c7b43be7957
JamesDaniel <jamesmcgarr@protonmail.com>
2017-07-05 15:52:55 +0100

    changed version of splunk to get access to more apps

--- splunk_start.sh
+++ splunk_start.sh
@@ -1,1 +1,1 @@
-docker run --name splunk -d -e "SPLUNK_START_ARGS=--accept-license" -e "SPLUNK_USER=root" -p "8000:8000" splunk/splunk
+docker run --name splunk -d -e "SPLUNK_START_ARGS=--accept-license" -e "SPLUNK_USER=root" -p "8000:8000" splunk/splunk:6.5.0
